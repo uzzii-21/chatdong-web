6ccb54173369a639083ed6551d8c7a0940bc2910
Uzair Ahmed <uzairahmed74372@gmail.com>
2021-10-28 23:04:55 +0500

    working oin firebase

--- src/components/UploadButton.js
+++ src/components/UploadButton.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 const UploadButton = () => {
-    
+    const [files, setFiles] = useState(null);
+    const type = ['image/png', 'image/jpeg', 'image/jpg', 'image/jxr', 'image/webp']
+    const changeHandler = (e) => {
+        let selected = e.target.files;
+         if(selected && type.includes(selected.type)){
+             setFiles(...selected);
+             console.log(files, "yes")
+         }
+         else{
+             setFiles(null)
+         }
+    }
     return (
-        <>
-        </>
+        <div>
+            <input type="file" onChange={changeHandler} multiple />
+            {/* {files} */}
+        </div>
     )
 }
 
