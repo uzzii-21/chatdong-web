4bf2c9edbe483215120d5b1e2d66b30b35ebbb86
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-01-06 21:24:00 +0500

    removed extra componenets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import Navbar from "./components/Navbar/Navbar";
-import UploadButton from "./components/UploadButton";
+import { useState } from "react";
+import DragandDrop from "./components/DragandDrop/DragandDrop";
 
 function App() {
+  const [files, setFiles] = useState({});
+  const [error, setError] = useState(null);
+
+  const type = ['image/png', 'image/jpeg', 'image/jpg', 'image/jxr', 'image/webp']
+
+  const changeHandler = (e) => {
+    let selected = e.target.files[0];
+    console.log(selected);
+    if (selected && type.includes(selected.type)) {
+      setFiles(selected);
+      setError(null)
+    }
+    else {
+      setFiles(null)
+      setError('Please Select an image file')
+    }
+  }
+  console.log(error);
   return (
     <div className="max-w-screen-xl mx-auto">
-      <Navbar />
-      <UploadButton />
+      <div>
+        <DragandDrop uploadFile={changeHandler} />
+        <div>{error ? error : files.name}</div>
+      </div>
     </div>
   );
 }
