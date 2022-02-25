33c62be543d2adf6a5942be0b50f0b4469687aa2
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-02-25 18:30:21 +0500

    cleaneup folders

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import { useState } from "react";
-import DragAndDrop from "./components/DragAndDrop/DragAndDrop";
+const App = () => (
+  <div className="App">
+    <h1 className="text-9xl">Uzair</h1>
+  </div>
+)
 
-function App() {
-  const [files, setFiles] = useState({});
-  const [error, setError] = useState(null);
-
-  const type = ['image/png', 'image/jpeg', 'image/jpg', 'image/jxr', 'image/webp']
-
-  const changeHandler = (e) => {
-    let selected = e.target.files[0];
-    console.log(selected);
-    if (selected && type.includes(selected.type)) {
-      setFiles(selected);
-      setError(null)
-    }
-    else {
-      setFiles(null)
-      setError('Please Select an image file')
-    }
-  }
-  return (
-    <div className="max-w-screen-xl mx-auto">
-      <div>
-        <DragAndDrop uploadFile={changeHandler} />
-        <div>{error ? error : files.name}</div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
+export default App
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,21 +1,21 @@
-import * as firebase from "firebase/app";
-import 'firebase/storage';
-import 'firebase/firestore';
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
 
 // firebase configuration
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
-  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
-  projectId:process.env.REACT_APP_PROJECT_ID,
-  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId:process.env.REACT_APP_APP_ID
-};
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+}
 
 // initialize firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
-const storage = firebase.storage();
-const fireStore = firebase.firestore();
+const storage = firebase.storage()
+const fireStore = firebase.firestore()
 
-export {storage, fireStore};
+export { storage, fireStore }
